9f31ba4fac3af964f2ade0fdaa31e8c8765c7e4d
ShinEunJin <sineun5501@naver.com>
2021-03-09 17:08:35 +0900

    add proxy

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "https://book.interpark.com/api/",
+    baseURL: "api/",
     params: {
         key: process.env.REACT_APP_API_KEY,
         output: 'json'
